384d20a22f70c267de7463cb417e5acc885f0829
Afwan Azman <96162282+afwanazman@users.noreply.github.com>
2024-08-30 11:33:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import logging
 import time
-from utils import create_log_group, create_target_group, get_next_priority, create_rule, register_task_definition, create_ecs_service, create_cname_record_cloudflare, save_deployment_info, elbv2_client
-from config import log_group, alb_arn, domain_name, cloudflare_api_token, cloudflare_zone_id, alb_dns_name
+import utils
+import config
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def main():
-    create_log_group(log_group)
-    target_group_arn = create_target_group()
+    utils.create_log_group(config.log_group)
+    target_group_arn = utils.create_target_group()
     if target_group_arn:
-        listener_response = elbv2_client.describe_listeners(LoadBalancerArn=alb_arn)
+        listener_response = utils.elbv2_client.describe_listeners(LoadBalancerArn=config.alb_arn)
         https_listener_arn = next(listener['ListenerArn'] for listener in listener_response['Listeners'] if listener['Port'] == 443)
         rules_list = []
-        create_rule(https_listener_arn, target_group_arn, domain_name, rules_list)
-        task_definition_arn = register_task_definition()
+        utils.create_rule(https_listener_arn, target_group_arn, config.domain_name, rules_list)
+        task_definition_arn = utils.register_task_definition()
         if task_definition_arn:
-            create_ecs_service(task_definition_arn, target_group_arn)
+            utils.create_ecs_service(task_definition_arn, target_group_arn)
             timestamp = time.strftime("%Y%m%d-%H%M%S")
-            create_cname_record_cloudflare(cloudflare_api_token, cloudflare_zone_id, domain_name, alb_dns_name)
-            save_deployment_info(task_definition_arn, target_group_arn, https_listener_arn, rules_list, timestamp)
+            utils.create_cname_record_cloudflare(config.cloudflare_api_token, config.cloudflare_zone_id, config.domain_name, config.alb_dns_name)
+            utils.save_deployment_info(task_definition_arn, target_group_arn, https_listener_arn, rules_list, timestamp)
 
 if __name__ == "__main__":
     main()
